4591ee79a20b5dadab0e5852790e5853700f037a
Ye Yint Thet Khine <yeyintthetkhine@my.jcu.edu.au>
2020-02-14 11:38:51 +0630

    Initial commit

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,47 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+      name: "Thant Sin Aung",
+      message: "Will You Come Today?",
+      time: "15:30",
+      avatarUrl:
+          "https://www.pngarts.com/files/3/Avatar-PNG-Download-Image.png"),
+  new ChatModel(
+      name: "Aung Zin Phyo",
+      message: "Hi!",
+      time: "17:30",
+      avatarUrl:
+          "https://www.pngarts.com/files/3/Avatar-PNG-Download-Image.png"),
+  new ChatModel(
+      name: "Sai Khun Kham",
+      message: "Bro, Give me your steam account",
+      time: "5:00",
+      avatarUrl:
+          "https://www.pngarts.com/files/3/Avatar-PNG-Download-Image.png"),
+  new ChatModel(
+      name: "Aung Zay Ya",
+      message: "Ko Ye Yint",
+      time: "10:30",
+      avatarUrl:
+          "https://www.pngarts.com/files/3/Avatar-PNG-Download-Image.png"),
+  new ChatModel(
+      name: "Khin Yadanar",
+      message: "Grades are out!!!",
+      time: "12:30",
+      avatarUrl:
+          "https://www.pngarts.com/files/3/Girl-Avatar-PNG-Image-Background.png"),
+  new ChatModel(
+      name: "Alfield",
+      message: "Kwee Assignment Lay Pay Par Own",
+      time: "15:30",
+      avatarUrl:
+          "https://www.pngarts.com/files/3/Avatar-PNG-Download-Image.png"),
+];
